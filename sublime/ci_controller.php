ad4e84c783b08a637e64b6ae643c9c3f9d797f19
quangthinh <nguyenthinh0408@gmail.com>
2014-05-24 00:52:18 -0700

    CI_Controller

---
+++ sublime/ci_controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Trang người dùng 
+ */
+class user extends CI_Controller
+{
+    var $data;
+    function __construct()
+    {
+    	parent::__construct();
+        $this->data = array();
+    }
+
+}
